31d2910a199d88179544a588778ab5f9bf1630ff
Coetzee <P10493207@capita.co.uk>
2019-05-08 15:33:40 +0100

    Add expensify styles

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -17,12 +17,22 @@ export class EditExpensePage extends React.Component {
   render() {
     return (
     <div>
+      <div className="page-header">
+        <div className="content-container">
+          <h1 className="page-header__title">Edit Expense</h1>
+        </div>
+      </div>
+      <div className="content-container">
       <ExpenseForm
         expense={this.props.expense}
         onSubmit={this.onSubmit}
       />
+      </div>
+      <div className="content-container">
       <button 
-        onClick={this.onClick}>Remove</button>
+        className="button button--secondary"
+        onClick={this.onClick}>Remove Expense</button>
+      </div>
     </div>
     )
   }
